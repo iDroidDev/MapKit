9aa08a41fb6acbdaa36ecb2c28db0a891e23c9e0
Kaan KÜN <kaan.kun@appcent.mobi>
2020-03-04 21:39:45 +0300

    -BugFix for Maps

---
+++ app/src/main/java/idroid/android/mapkit/Application.kt
@@ -0,0 +1,12 @@
+package idroid.android.mapkit
+
+
+import android.app.Application
+
+class Application : Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+
+
+}
--- app/src/main/java/idroid/android/mapkit/MainActivity.kt
+++ app/src/main/java/idroid/android/mapkit/MainActivity.kt
@@ -4,20 +4,21 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
+
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        var mapViewBundle: Bundle? = null
-        if (savedInstanceState != null) {
-            mapViewBundle = savedInstanceState.getBundle("MapViewBundleKey")
+        var mapViewBundle = savedInstanceState?.getBundle("MapViewBundleKey")
+        if (mapViewBundle == null) {
+            mapViewBundle = Bundle()
+            savedInstanceState?.putBundle("MapViewBundleKey", mapViewBundle)
         }
-        maps.onCreate(mapViewBundle!!)
 
-        maps.getMapAsync {
-            it.setMyLocationEnabled(true)
+        mapView.onCreate(mapViewBundle)
+        mapView.getMapAsync {
+            //it.setMyLocationEnabled(true)
             it.moveCamera(41.000000F, 29.00000F, 20f)
         }
     }
